5772cce722546f66873aea261a5a5351d2470d60
SMJNayeem <jannatulnayeem018@gmail.com>
2024-05-25 14:17:27 +0600

    feat:alertservice added

--- server2/app.js
+++ server2/app.js
@@ -1,21 +1,29 @@
 const express = require('express');
-const promClient = require('prom-client');
+const client = require('prom-client');
 const app = express();
 const port = process.env.PORT || 3002;
 
-const requestCount = new promClient.Counter({
+const register = new client.Registry();
+const counter = new client.Counter({
   name: 'request_count',
-  help: 'The total number of requests'
+  help: 'Number of requests received',
 });
 
+register.registerMetric(counter);
+
 app.get('/login', (req, res) => {
-  requestCount.inc();
+  counter.inc();
   res.send('Login endpoint');
 });
 
 app.get('/metrics', async (req, res) => {
-  res.set('Content-Type', promClient.register.contentType);
-  res.end(await promClient.register.metrics());
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
+app.get('/scale', (req, res) => {
+  // Simulate scaling logic
+  res.send('Scaling endpoint hit');
 });
 
 app.listen(port, () => {
