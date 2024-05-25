203659a007202dc415a2bbb239c76734ac962fb7
SMJNayeem <jannatulnayeem018@gmail.com>
2024-05-25 13:40:56 +0600

    login counter with prometheus and alertmanager

---
+++ server1/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const promClient = require('prom-client');
+const app = express();
+const port = process.env.PORT || 3001;
+
+const requestCount = new promClient.Counter({
+  name: 'request_count',
+  help: 'The total number of requests'
+});
+
+app.get('/login', (req, res) => {
+  requestCount.inc();
+  res.send('Login endpoint');
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(await promClient.register.metrics());
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
